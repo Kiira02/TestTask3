f3b76b156ae6f3207079504b68d1a161822b5cad
Kiira02 <Kiira005@gmail.com>
2013-09-20 18:49:46 +0300

    fixed animation and routing functional

--- myslider.js
+++ myslider.js
@@ -1,16 +1,20 @@
 YUI.add('myslider', function (Y) {
 
     function mySlider(config) {
-        var mySliderList, app;
+        var mySliderList, app,
+		    object = config['content'],
+            currentPage = 0;
 
         Y.SliderModel = Y.Base.create('sliderModel', Y.Model, [], {
-            test: function () {
-                //to do
-            }
+		nextPage: function () {
+			app.navigate('/:next');
+		}
         }, {
             ATTRS: {
                 title: { value: '' },
-                content: { value: '' }
+                content: { value: '' },
+		    button: { value: '<button class="prev">PREV</button><button class="next">NEXT</button>'},
+		    name: { value: ''}
             }
         });
 
@@ -24,43 +28,47 @@ YUI.add('myslider', function (Y) {
                 return numPages;
             },
             getPage: function (page) {
-                var filteredPage = this.filter(function (item) {
-                    return item.get('title') === page;
-                });
-                if (filteredPage.length !== 0) {
-                    return filteredPage[0];
-                }
+                    return this._items[page];
             }
         });
 
         Y.SliderView = Y.Base.create('sliderView', Y.View, [], {
-            events: { '#demo' : {click: function() {app.navigate('/next'); app.render()} }},
-            template: '<div>{content}</div><button id="demo">TEST</button>',
+            events: {
+		    '.next' : { click: 'nextPage' },
+		    '.prev' : { click: 'prevPage'}
+		},
+		template: Y.one('#templateForSlider').getHTML(),
             initializer: function () {
                 var model = this.get('model');
-                model.after('change', this.render, this);
-                model.after('destroy', this.render, this);
+                	  model.after('change', this.render, this);
+               	  model.after('destroy', this.render, this);
             },
             render: function () {
-                var container = this.get('container'),
-                    data = { content: this.get('model').toJSON().content },
-                    contData = Y.Lang.sub(this.template, data);
+			var container = this.get('container'),
+			    model = this.get('model'),
+			    data = {content: this.get('model').toJSON().content, buttons: this.get('model').toJSON().button, title: this.get('model').toJSON().title},
+			    contData = Y.Lang.sub(this.template, data);
 
-                container.setHTML(contData);
-                if (! container.inDoc()) {
-                    Y.one('body').append(container);
-                }
-                return this;
-            }
-        }, {
-            ATTRS: {
-                container: { value: Y.Node.create('<div/>')}
-            }
+		    	container.setHTML(contData);
+		    	Y.one('body').appendChild(container);
+            },
+		nextPage: function () {
+            var numPages = mySliderList.getTotalPages() - 1;
+            currentPage++;
+            if(currentPage > numPages) currentPage = 0;
+            app.navigate('/' + mySliderList._items[currentPage].get('name'));
+		},
+		prevPage: function () {
+            var numPages = mySliderList.getTotalPages() - 1;
+            currentPage--;
+            if(currentPage < 0) currentPage = numPages;
+			app.navigate('/' + mySliderList._items[currentPage].get('name'));
+		}
         });
 
         mySliderList = new Y.SliderList();
-        for (var i in config['content']) {
-            mySliderList.add([{ title: config['content'][i].title, content: config['content'][i].content }]);
+        for (var i in object) {
+            mySliderList.add([{ title: object[i].title, content: object[i].content, name: i }]);
         }
 
         app = new Y.App({
@@ -69,25 +77,29 @@ YUI.add('myslider', function (Y) {
             views: {
                 slider: {
                     type: 'SliderView'
-                }
+                },
+		    slider2: {
+			    type: 'SlideView',
+			    parent: 'SliderView'
+		    }
             },
             routes: [{
                 path: '/',
                 callback: function () {
-                    var pageModel = mySliderList.getPage('Pagetitle');
+                    var pageModel = mySliderList.getPage(currentPage);
                     this.showView('slider', { model: pageModel });
                 }
             }, {
                 path: '/:pageName',
-                callback: function () {
-                    var pageModel = mySliderList.getPage('Pagetitle2');
+                callback: function (req) {
+                    var pageModel = mySliderList.getPage(currentPage);
                     this.showView('slider', { model: pageModel });
                 }
             }]
         });
 
         app.navigate('/');
-	  app.render();
+	    app.render();
     };
 
     Y.namespace('MySlider').Start = function (config) {
